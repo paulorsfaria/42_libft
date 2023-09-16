3f0637c7b6a714f8710f0daef27ce0ee45f0cc91
paulorsfaria <paulo.esaf@gmail.com>
2023-09-16 18:04:45 +0100

    ft_isalpha, ft_isdigit, ft_isascii, ft_isprint, ft_strlen, ft_toupper, ft_tolower, ft_atoi

---
+++ ft_atoi.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paulo-do <paulo-do@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/16 16:24:13 by paulo-do          #+#    #+#             */
+/*   Updated: 2023/09/16 16:36:21 by paulo-do         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	atoi(const char *str)
+{
+	int	i;
+	int	sinal;
+	int	result;
+
+	i = 0;
+	sinal = 1;
+	result = 0;
+	while(str[i] != '\0')
+	{
+		while(str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+			i++;
+		if(str[i] == '-')
+		{
+			sinal *= -1;
+			i++;
+		}
+		while(str[i] >= '0' && str[i] <= '9')
+		{
+			result = (result * 10) + (str[i] - '0');
+			i++;
+		}
+		return (result * sinal);
+	}
+	return(0);
+	
+}
