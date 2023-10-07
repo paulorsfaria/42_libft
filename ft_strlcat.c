974a06817bf6031f2acb9ff9891fa7062f03d487
Paulo Ricardo Dos Santos Faria <paulo-do@c1r4s2.42porto.com>
2023-10-07 10:48:25 +0100

    fixed errors

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: paulo-do <paulo-do@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/23 15:18:41 by paulo-do          #+#    #+#             */
-/*   Updated: 2023/10/02 14:27:16 by paulo-do         ###   ########.fr       */
+/*   Updated: 2023/10/07 10:47:48 by paulo-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,21 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	i;
-	size_t	j;
-	size_t	sleng;
 	size_t	dleng;
+	size_t	sleng;
 
 	i = 0;
 	dleng = ft_strlen(dst);
-	j = dleng;
 	sleng = ft_strlen(src);
 	if (size == 0)
-		return (ft_strlen(src));
-	if (size < sleng)
+		return (sleng);
+	if (size <= dleng)
 		return (sleng + size);
-	while ((src[i] != '\0') && (i < dleng - size -1))
+	while (src[i] != '\0' && i < size - dleng - 1)
 	{
-		dst[j] = src[i];
+		dst[dleng + i] = src[i];
 		i++;
-		j++;
 	}
-	dst[sleng] = '\0';
-	return (dleng + ft_strlen(src));
+	dst[dleng + i] = '\0';
+	return (dleng + sleng);
 }
