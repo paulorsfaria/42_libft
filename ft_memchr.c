b3a4434c3b156da8454dcc127fb40ff08f372719
paulorsfaria <paulofaria.esaf@gmail.com>
2023-10-07 17:40:28 +0100

    added

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,24 @@
 /*   By: paulo-do <paulo-do@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 14:41:05 by paulo-do          #+#    #+#             */
-/*   Updated: 2023/10/07 14:41:06 by paulo-do         ###   ########.fr       */
+/*   Updated: 2023/10/07 16:09:33 by paulo-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t	i;
+	unsigned char	*str;
+
+	str = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		if (str[i] == ((unsigned char )c))
+			return (&str[i]);
+		i++;
+	}
+	return (NULL);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: paulo-do <paulo-do@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 11:21:31 by paulo-do          #+#    #+#             */
-/*   Updated: 2023/10/07 11:35:41 by paulo-do         ###   ########.fr       */
+/*   Updated: 2023/10/07 15:50:31 by paulo-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,5 +45,5 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 char	*ft_strrchr(const char *s, int c);
 int	ft_memcmp(const void *s1, const void *s2, size_t n);
-
+void	*ft_memchr(const void *s, int c, size_t n);
 #endif
