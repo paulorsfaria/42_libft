f48d1811f6b0a82e37b885863fce734eb1d36958
paulorsfaria <paulofaria.esaf@gmail.com>
2023-10-08 18:16:01 +0100

    ta quase

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,13 +6,25 @@
 /*   By: paulo-do <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 16:24:45 by paulo-do          #+#    #+#             */
-/*   Updated: 2023/10/08 16:34:09 by paulo-do         ###   ########.fr       */
+/*   Updated: 2023/10/08 18:15:25 by paulo-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-	
+	size_t	i;
+	size_t	start;
+
+	i = 0;
+	if (!s1 || !set)
+		return (NULL);
+	while (ft_strchr(set, s1[i]))
+		i++;
+	start = i;
+	i = ft_strlen(s1) - 1;
+	while (ft_strchr(set, s1[i]))
+		i--;
+	return (ft_substr(s1, start, i - start + 1));
 }
