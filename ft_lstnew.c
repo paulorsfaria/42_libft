c5bc31f18873ec450b29d6877400ac8dd52451d7
paulorsfaria <paulo.esaf@gmail.com>
2023-10-30 21:00:42 +0000

    a

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstnew.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: paulo-do <paulo-do@student.42.fr>          +#+  +:+       +#+        */
+/*   By: paulo-do <paulo-do@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/15 17:09:58 by paulo-do          #+#    #+#             */
-/*   Updated: 2023/10/30 12:45:09 by paulo-do         ###   ########.fr       */
+/*   Updated: 2023/10/30 20:54:21 by paulo-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,3 +23,21 @@ t_list	*ft_lstnew(void *content)
 	node->next = NULL;
 	return (node);
 }
+/*
+int main() {
+    // Test the ft_lstnew function
+    int *data = (int *)malloc(sizeof(int));
+    *data = 42; // Example data to store in the list node
+
+    t_list *new_node = ft_lstnew(data);
+
+    if (new_node) {
+        printf("New Node Content: %d\n", *((int *)(new_node->content)));
+        free(new_node->content);
+        free(new_node);
+    } else {
+        printf("Failed to create a new node.\n");
+    }
+
+    return 0;
+}*/
